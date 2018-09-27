8a6f1fc378e54a1ec296e4e6b01ae891b3bec77b
jirenguwangfei <1208321826@qq.com>
2018-09-27 17:51:07 +0800

    Initial commit

---
+++ src/pages/Index/template.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="index">
+    <p> 博客首页 </p>
+    <div>
+      <el-button @click="onClick1">默认按钮</el-button>
+      <el-button @click="onClick2">alert</el-button>
+    </div>
+  </div>
+</template>
+
+<script src="./template.js"></script>
+
+<style src="./template.css"></style>
